cc442fa455deee37487811e62f61191c17dc0aab
mahemel <mahbubhasanhemel@gmail.com>
2026-04-16 04:47:52 +0600

    updated ui of errorpage

--- src/pages/errorpage/Errorpage.jsx
+++ src/pages/errorpage/Errorpage.jsx
@@ -6,19 +6,19 @@ const Errorpage = () => {
     return (
         <>
             <Navbar></Navbar>
-            <div className="text-center space-y-6 py-20">
-                <h1 className="text-8xl font-extrabold text-primary">404</h1>
+            <div className="text-center space-y-6 py-20 px-5">
+                <h1 className="text-7xl font-extrabold text-dark-green">404</h1>
 
-                <h2 className="text-2xl md:text-3xl font-semibold">
+                <h2 className="text-2xl md:text-3xl font-semibold text-dark-black">
                     Oops! Page not found
                 </h2>
-                <p className="text-base-content/70 max-w-md mx-auto">
+                <p className="text-light-gray max-w-md mx-auto">
                     The page you’re looking for doesn’t exist or has been moved.
                 </p>
 
                 <div className="flex flex-col sm:flex-row gap-3 justify-center">
-                    <a href="/" className="btn btn-primary">
-                        Go Home
+                    <a href="/" className="btn bg-dark-green text-white">
+                        Go Back to Home
                     </a>
                 </div>
             </div>
